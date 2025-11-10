60062ae4be1a899d045b00ab7ea969c998b4eaf0
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-11-10 15:50:36 -0500

    added Topping class with name amd price

--- src/main/java/com/pizza/model/Topping.java
+++ src/main/java/com/pizza/model/Topping.java
@@ -1,5 +1,31 @@
 package com.pizza.model;
 
 public class Topping {
+    private  String name;
+    private double price;
 
+
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return  name + " ($" + String.format("%.2f", price) + ")";
+    }
 }
