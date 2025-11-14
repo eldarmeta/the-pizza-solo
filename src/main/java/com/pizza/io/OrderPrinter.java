cd5800de5b48ae05d8a0b0ed67f80bc8649ef921
Eldar Abdivaitov <eldarmeta@outlook.com>
2025-11-13 20:12:35 -0500

    refactor(io): made OrderPrinter implement ReceiptWriter and use util helpers

--- src/main/java/com/pizza/io/OrderPrinter.java
+++ src/main/java/com/pizza/io/OrderPrinter.java
@@ -5,17 +5,16 @@ import com.pizza.model.Pizza;
 import com.pizza.model.Topping;
 import com.pizza.model.Drink;
 import com.pizza.model.SideItem;
+import com.pizza.util.DateUtil;
+import com.pizza.util.PriceFormatter;
 
-import java.time.format.DateTimeFormatter;
+public class OrderPrinter implements ReceiptWriter {
 
-public class OrderPrinter {
-
-    public void printToConsole(Order order) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
+    @Override
+    public void write(Order order) {
         System.out.println("=== Order Receipt ===");
         System.out.println("Order ID: " + order.getOrderId());
-        System.out.println("Created at: " + order.getCreatedAt().format(formatter));
+        System.out.println("Created at: " + DateUtil.formatForDisplay(order.getCreatedAt()));
         System.out.println();
 
         int pizzaNumber = 1;
@@ -24,9 +23,9 @@ public class OrderPrinter {
             System.out.println("Size: " + pizza.getSize());
             System.out.println("Toppings:");
             for (Topping topping : pizza.getToppings()) {
-                System.out.println("- " + topping.getName() + " ($" + String.format("%.2f", topping.getPrice()) + ")");
+                System.out.println("- " + topping.getName() + " (" + PriceFormatter.format(topping.getPrice()) + ")");
             }
-            System.out.println("Pizza Total: $" + String.format("%.2f", pizza.calculatePrice()));
+            System.out.println("Pizza Total: " + PriceFormatter.format(pizza.calculatePrice()));
             System.out.println();
             pizzaNumber++;
         }
@@ -34,7 +33,7 @@ public class OrderPrinter {
         if (!order.getDrinks().isEmpty()) {
             System.out.println("Drinks:");
             for (Drink drink : order.getDrinks()) {
-                System.out.println("- " + drink.getName() + " ($" + String.format("%.2f", drink.getPrice()) + ")");
+                System.out.println("- " + drink.getName() + " (" + PriceFormatter.format(drink.getPrice()) + ")");
             }
             System.out.println();
         }
@@ -42,12 +41,12 @@ public class OrderPrinter {
         if (!order.getSides().isEmpty()) {
             System.out.println("Sides:");
             for (SideItem sideItem : order.getSides()) {
-                System.out.println("- " + sideItem.getName() + " ($" + String.format("%.2f", sideItem.getPrice()) + ")");
+                System.out.println("- " + sideItem.getName() + " (" + PriceFormatter.format(sideItem.getPrice()) + ")");
             }
             System.out.println();
         }
 
-        System.out.println("TOTAL ORDER: $" + String.format("%.2f", order.getTotal()));
+        System.out.println("TOTAL ORDER: " + PriceFormatter.format(order.getTotal()));
         System.out.println("Thank you for ordering!");
     }
 }
